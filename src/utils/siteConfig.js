3bf363a5d1ed969b48fccfc74e98f53e47877db0
Bitpresse <111120848+bitpresse@users.noreply.github.com>
2022-09-16 20:22:50 +0100

    Update siteConfig.js

--- src/utils/siteConfig.js
+++ src/utils/siteConfig.js
@@ -2,19 +2,19 @@ const config = require(`../../.ghost.json`).production;
 module.exports = {
     siteUrl:
         process.env.NODE_ENV === `production`
-            ? process.env.SITE_URL || config.siteUrl || `http://localhost:8000`
-            : `http://localhost:8000`, // Site domain. Do not include a trailing slash!
+            ? process.env.SITE_URL || config.siteUrl || `https://bitpresse.com`
+            : `https://www.bitpresse.com`, // Site domain. Do not include a trailing slash!
 
-    postsPerPage: 12, // Number of posts shown on paginated pages (changes this requires sometimes to delete the cache)
+    postsPerPage: 100, // Number of posts shown on paginated pages (changes this requires sometimes to delete the cache)
 
-    siteTitleMeta: `Ghost Gatsby Starter`, // This allows an alternative site title for meta data for pages.
-    siteDescriptionMeta: `A starter template to build amazing static websites with Ghost and Gatsby`, // This allows an alternative site description for meta data for pages.
+    siteTitleMeta: `Bitpresse`, // This allows an alternative site title for meta data for pages.
+    siteDescriptionMeta: `Bitpresse est le plus grand contenu crypto en français sur le web, allant de l'actualité crypto à la blockchain, bitcoin, portefeuilles, bots et échanges.`, // This allows an alternative site description for meta data for pages.
 
-    shareImageWidth: 1000, // Change to the width of your default share image
-    shareImageHeight: 523, // Change to the height of your default share image
+    shareImageWidth: 1200, // Change to the width of your default share image
+    shareImageHeight: 630, // Change to the height of your default share image
 
-    shortTitle: `Ghost`, // Used for App manifest e.g. Mobile Home Screen
+    shortTitle: `Bitpresse`, // Used for App manifest e.g. Mobile Home Screen
     siteIcon: `favicon.png`, // Logo in /static dir used for SEO, RSS, and App manifest
     backgroundColor: `#e9e9e9`, // Used for Offline Manifest
-    themeColor: `#15171A`, // Used for Offline Manifest
+    themeColor: `#016EB1`, // Used for Offline Manifest
 };
